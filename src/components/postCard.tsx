b0568d493abc8968e7dab467c39b7d1b4dd9888b
RaviAnand Mohabir <moravrav@gmail.com>
2023-05-08 00:01:55 +0200

    feat: :lipstick: improve responsive design of post card

--- src/components/postCard.tsx
+++ src/components/postCard.tsx
@@ -1,7 +1,7 @@
 import { usePocketBase } from "@/pocketbase";
 import { Post } from "@/pocketbase/models";
 import { Carousel } from "@mantine/carousel";
-import { Badge, Box, CardProps, Image, Paper, Title } from "@mantine/core";
+import { CardProps, Flex, Image, Paper, Stack } from "@mantine/core";
 import { IMAGE_MIME_TYPE } from "@mantine/dropzone";
 import Link from "next/link";
 import { Record } from "pocketbase";
@@ -27,32 +27,51 @@ function PostCard({ post, ...props }: PostCardProps) {
       })}
       bg="dark.6"
       radius="md"
+      miw={0}
     >
-      <PostTitle post={post} p="md" />
-      <Carousel withIndicators onClickCapture={(ev) => ev.preventDefault()}>
-        {Array.isArray(post.expand.files) &&
-          post.expand.files.map((f) => (
-            <Carousel.Slide key={f.id}>
-              {IMAGE_MIME_TYPE.includes(f.type) ? (
-                <Image
-                  src={pb.files.getUrl(f, f.file)}
-                  sx={{ flex: 1 }}
-                  alt={
-                    post.title ||
-                    `Post by ${(post.expand.author as Record).username}`
-                  }
-                />
-              ) : (
-                <video
-                  src={pb.files.getUrl(f, f.file)}
-                  muted
-                  autoPlay
-                  controls={false}
-                />
-              )}
-            </Carousel.Slide>
-          ))}
-      </Carousel>
+      <Stack mah="70vh">
+        <PostTitle post={post} p="md" />
+        <Flex mih={0} w="100%" miw={0}>
+          <Carousel
+            withIndicators
+            onClickCapture={(ev) => ev.preventDefault()}
+            height="100%"
+            w="100%"
+          >
+            {Array.isArray(post.expand.files) &&
+              post.expand.files.map((f) => (
+                <Carousel.Slide key={f.id}>
+                  <Flex align="center" h="100%" w="100%" justify="center">
+                    {IMAGE_MIME_TYPE.includes(f.type) ? (
+                      <Image
+                        src={pb.files.getUrl(f, f.file)}
+                        sx={{ flex: 1 }}
+                        alt={
+                          post.title ||
+                          `Post by ${(post.expand.author as Record).username}`
+                        }
+                      />
+                    ) : (
+                      <video
+                        src={pb.files.getUrl(f, f.file)}
+                        muted
+                        autoPlay
+                        controls={false}
+                        style={{
+                          objectFit: "contain",
+                          objectPosition: "center",
+                          maxHeight: "100%",
+                          margin: "0 auto",
+                          display: "block",
+                        }}
+                      />
+                    )}
+                  </Flex>
+                </Carousel.Slide>
+              ))}
+          </Carousel>
+        </Flex>
+      </Stack>
     </Paper>
   );
 }
