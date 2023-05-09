f4fe555de46261ab9c54361e0c5e3d29550d4a8b
RaviAnand Mohabir <moravrav@gmail.com>
2023-05-09 09:21:09 +0200

    feat: :sparkles: add following relation to users table

---
+++ server/migrations/1683555032_updated_users.go
@@ -0,0 +1,54 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models/schema"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// add
+		new_following := &schema.SchemaField{}
+		json.Unmarshal([]byte(`{
+			"system": false,
+			"id": "5jigwknv",
+			"name": "following",
+			"type": "relation",
+			"required": false,
+			"unique": false,
+			"options": {
+				"collectionId": "_pb_users_auth_",
+				"cascadeDelete": false,
+				"minSelect": null,
+				"maxSelect": null,
+				"displayFields": []
+			}
+		}`), new_following)
+		collection.Schema.AddField(new_following)
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("_pb_users_auth_")
+		if err != nil {
+			return err
+		}
+
+		// remove
+		collection.Schema.RemoveField("5jigwknv")
+
+		return dao.SaveCollection(collection)
+	})
+}
